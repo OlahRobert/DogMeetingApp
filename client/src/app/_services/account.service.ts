bf9ce19b342390881e21bcf8737509671ee637a5
Robert Olah <olahrobert21@gmail.com>
2022-08-16 18:32:22 +0300

    Page for members

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,17 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AccountService {
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = environment.apiUrl;
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   login(model: any) {
     return this.http.post(this.baseUrl + 'account/login', model).pipe(
@@ -34,9 +35,8 @@ export class AccountService {
           this.currentUserSource.next(user);
         }
       })
-    )
+    );
   }
-
 
   setCurrentUser(user: User) {
     this.currentUserSource.next(user);
